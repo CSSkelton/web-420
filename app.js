f4b34d762ba618a6a4d421bd6ea61fd08c456315
CSSkelton <codysskelton@gmail.com>
2024-01-13 22:40:43 -0500

    Application Setup

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+var mongoose = require('mongoose');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'WEB 420 RESTful APIs',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./routes/*.js'], // Files containing annotations for the OpenAPI Specification
+};
+
+const openapiSpecification = swaggerJSDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log(`Application started and listening on port ${app.get('port')}`);
+})
